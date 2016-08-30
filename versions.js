fbf00713b1d99499b5366800f2385baa88f691cf
glottotopia <sebastian.nordhoff@glottotopia.de>
2016-08-30 14:36:47 +0200

    add versions

---
+++ versions.js
@@ -0,0 +1,18 @@
+{
+  "versions": [{
+    "versiontype": "openreview",
+    "title": "Test document open review",
+    "authors": [{"name": "Esther Tester"}],
+    "publishedAt": "2016-07-25T11:00:00.000Z"
+  }, {
+    "versiontype": "proofreading",
+    "title": "Test document proofreading",
+    "authors": [{"name": "Esther Tester"}],
+    "publishedAt": "2016-08-15T00:22:00.000Z"
+  }, {
+    "versiontype": "main",
+    "title": "Test document main",
+    "authors": [{"name": "Esther Tester"}],
+    "publishedAt": "2016-08-25T00:00:33.000Z"
+  }]
+}
